193514df89b400fb08772379acebb728805fda88
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-05-03 14:18:51 -0500

    Initial password hashing with set salt

--- rainbow.py
+++ rainbow.py
@@ -3,11 +3,12 @@ import hashlib
 # For databases
 import json
 # For commandline args
-import sys
 import argparse
 
 # Get command line arguments
 inputFilePath = ""
+outputFilePath = "output.json"
+salt = "noSaltYet :)"
 
 parser = argparse.ArgumentParser()
 
@@ -22,3 +23,32 @@ else:
     inputFilePath = "input.json"
     print("No input provided, defaulting to input.json")
 
+# open the plaintext file as read only
+plaintextFile = open(inputFilePath)
+# read it into a dictionary
+plaintext = json.loads(plaintextFile.read())
+
+# hashed dictionary
+hashed = {
+    "accounts": []
+}
+hashedTemplate = {
+        "username": "",
+        "salt": "",
+        "hash": ""
+}
+
+# for each account in accounts
+for account in plaintext["accounts"]:
+    newAccount = hashedTemplate
+    newAccount["username"] = account["username"]
+    newAccount["salt"] = salt
+    # hash the plaintext password NOTE: This configuration is insecure for testing purposes
+    hash = hashlib.sha256(hashlib.pbkdf2_hmac('sha256', account["password"].encode(), salt.encode(), 1)).hexdigest()
+    newAccount["hash"] = hash
+    # append the hashed account to hashed
+    hashed["accounts"].append(newAccount)
+
+# output hashed to the output json file
+hashedJson = json.dumps(hashed)
+print(hashedJson)
