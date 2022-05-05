e1fb47e73380ad568f796bede32cefd03cfe13e7
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-05-05 14:31:51 -0500

    Improved arg documentation

--- rainbow.py
+++ rainbow.py
@@ -12,11 +12,11 @@ rounds = 1
 parser = argparse.ArgumentParser()
 
 # Add arguments
-parser.add_argument("-i", "--Input", help = "Input database file with plaintext passwords for hashing, or hashed password for brute forcing")
-parser.add_argument("-d", "--Dictionary", help = "File path to the dictionary file used for a dictionary attack")
+parser.add_argument("-i", "--Input", help = "Input database file with plaintext passwords for hashing.")
+parser.add_argument("-d", "--Dictionary", help = "File path to the dictionary file used for brute forcing.")
 # Add arguments
-parser.add_argument("-ha", "--Hash", help = "Tells rainbow.py to create a hashed password json file, if both this", action="store_true")
-parser.add_argument("-r", "--Rainbow", help = "Tells rainbow.py to attempt to brute force database.json using the provided dicitonary", action="store_true")
+parser.add_argument("-ha", "--Hash", help = "Tells rainbow.py to create a json file with hashed passwords.", action="store_true")
+parser.add_argument("-r", "--Rainbow", help = "Tells rainbow.py to attempt to brute force database.json using the provided dicitonary.", action="store_true")
 parser.add_argument("-n", "--Rounds", help="Number of rounds to use for the hashing algorithm.")
 args = parser.parse_args()
 
